2728f849ce7a9262617b3d65e5a7b167b65ad61a
slavic-sky <44680193+slavic-sky@users.noreply.github.com>
2020-07-23 22:42:10 +0300

    some layout progress

---
+++ app/src/main/java/com/example/dogbreedslist/ui/dog/DogFragment.kt
@@ -0,0 +1,7 @@
+package com.example.dogbreedslist.ui.dog
+
+import androidx.fragment.app.Fragment
+
+class DogFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/dogbreedslist/ui/list/ListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.dogbreedslist.ui.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class DogFragment : Fragment() {
+
+}
